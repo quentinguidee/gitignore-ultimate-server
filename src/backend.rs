dd2c58226424f5467cfd40bfe476b37044558fa0
Quentin Guidée <quentin.guidee@gmail.com>
2022-05-25 14:00:29 +0200

    Create basic server with tower_lsp
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

---
+++ src/backend.rs
@@ -0,0 +1,31 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer};
+
+#[derive(Debug)]
+pub struct Backend {
+    client: Client,
+}
+
+impl Backend {
+    pub fn new(client: Client) -> Self {
+        Backend { client }
+    }
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "Server initialized successfully.")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+mod backend;
+
+use backend::Backend;
+use tokio::io::{stdin, stdout};
+use tower_lsp::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    let stdin = stdin();
+    let stdout = stdout();
+
+    let (service, socket) = LspService::new(|client| Backend::new(client));
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
